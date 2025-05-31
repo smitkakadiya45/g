594568c07db6b9671b43de912760035159fd857a
smitkakadiya45 <smitkakadiya45@gmail.com>
2025-05-31 09:41:00 +0530

    commit

--- server/config/connect.js
+++ server/config/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 async function connect () {
-    mongoose.connect('mongodb+srv://geetakakadiya299:geetakakadiya299@cluster0.u4low62.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect('mongodb+srv://geetakakadiya299:DGRbdWD06pthUmQW@cluster0.u4low62.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connected!'));
 }
 
